46f385a45b66fef9ada960c37e85a8c980e4845e
Luis Gustavo Fernández Batres <elclon_fernanbrans@hotmail.com>
2016-09-02 00:39:49 -0600

    Version inicial (solo seed RC5)

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component }  from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>My first angular 2 app</h1>`
+})
+class AppComponent { }
+
+export { AppComponent };
